b5b0874aeb6d9d5f80b44300802f72eee0347f26
Michael Kreil <github@michael-kreil.de>
2023-02-14 22:53:07 +0100

    fixes

--- index.js
+++ index.js
@@ -35,38 +35,55 @@ app.get(/.*/, async (req, res) => {
 
 			let [metadata] = (await file.getMetadata());
 			let headers = {
-				'cache-control': 'max-age=' + (86400 * 30),
+				'cache-control': 'max-age=' + (86400 * 7),
 			}
 
 			if (metadata.contentType) headers['content-type'] = metadata.contentType;
 			if (metadata.size) headers['content-length'] = metadata.size;
 			if (metadata.etag) headers['etag'] = metadata.etag;
 
-			file.createReadStream().pipe(res.status(200));
+			res.set(headers);
+			res.status(200);
+
+			file.createReadStream().pipe(res);
 		}
 
 		async function sendFileList() {
 			let [files] = await bucket.getFiles({ prefix, autoPaginate: false, maxResults: 1000 });
-			files = files.map(f => {
-				let name = f.name;
+			files = files.map(file => {
+				let name = file.name;
 				if (!name.startsWith(prefix)) return;
 				name = name.slice(prefix.length);
+				if (name.length === 0) return;
+
 				if (name.endsWith('/')) {
 					// folder
 					if (name.slice(0,-1).includes('/')) return;
-					return `<a href="${name}">${name}</a><br>`;
+					return `<tr><td><a href="${name}">${name}</a></td><td></td><td></td><tr>`;
 				} else {
 					// file
 					if (name.includes('/')) return;
-					return `<a href="${name}">${name}</a><br>`;
+					let size = parseInt(file.metadata.size, 10);
+					size = Math.round(size/(1024*1024))+' MB';
+					return `<tr><td><a href="${name}">${name}</a></td><td>${size}</td><td>${file.metadata.timeCreated}</td><tr>`;
 				}
 			})
 			.filter(f => f);
 			let html = [
 				'<html>',
-				'<body>',
-				...files,
-				'</body>',
+					'<head>',
+						'<style>',
+							'body { font-family: sans-serif }',
+							'table { border-spacing: 5px; }',
+							'table td:nth-child(2) { text-align: right }',
+						'</style>',
+					'</head>',
+					'<body>',
+					'<table>',
+					'<tr><th>filename</th><th>size</th><th>date</th></tr>',
+					...files,
+					'</table>',
+					'</body>',
 				'</html>',
 			].join('\n');
 			res.status(200).send(html);
